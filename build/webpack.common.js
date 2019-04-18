475ab9c9648a1c4b6e5c6a016522fbce6f00b092
user <hu.wei@yanwei365.com>
2019-04-18 17:37:02 +0800

    demo

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 /*const webpack = require('webpack');*/
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 /*const UglifyJSPlugin = require('uglifyjs-webpack-plugin');*/
 const common = require('./webpack.common.js');
 module.exports = merge(common, {
